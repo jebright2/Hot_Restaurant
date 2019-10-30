cc52cc7e07b9509f958bb3612ab2b969348c4ca4
digitalsamsquanch <digital.samsquanch@protonmail.com>
2019-10-29 20:47:27 -0400

    added wait and table data files with some dummy data

--- hotEats.js
+++ hotEats.js
@@ -24,13 +24,17 @@ var table = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "index.html"));
-    return res.json(table)
+    res.sendFile(path.join(__dirname, "index.html"));
+    // return res.json(table)
 });
 
 app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
 
 app.post("/api/reserve", function(req, res) {
   var newCharacter = req.body;
